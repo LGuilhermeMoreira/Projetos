51bd11a37c8fd3b87070eb0055d0e6f483823699
Guilhermemoreia <Lguilhermemoreiraleite@gmail.com>
2022-06-24 17:34:41 -0300

    Update pedrapapeltesoura.c
    Versão final

--- projects/pedrapapeltesoura.c
+++ projects/pedrapapeltesoura.c
@@ -1,16 +1,24 @@
 #include<time.h>
 #include<stdio.h>
 #include<stdlib.h>
+
+char jogada_bot(){
+   srand(time(NULL));
+   int resp = rand()%3;
+   
+   if(resp == 0) return 'R'; // pedra
+   else if(resp == 1) return 'P'; // papel   
+   else return 'S'; // tesoura
+}
+
 int main()
 {
-   // lendo o nome dos jogadores
+   // lendo o nome do jogadore
    printf("Digite o nome do primeiro jogador: ");
    char nome1[100];
    scanf("%[^\n]",nome1);
-   printf("Digite o nome do segundo jogador: ");
-   char nome2[100];
-   scanf(" %[^\n]",nome2);
-   
+   printf("\n");
+  
    for(int i = 1; i =! 0;i++){
       if(i == 1){
          // inicializando o jogo
@@ -21,25 +29,28 @@ int main()
       printf("\n");
 
       // lendo os mãos dos jogadores
-      char jogador1,jogador2;
+      char jogador;
       printf("Digite a escolha do primeiro jogador: ");
-      scanf(" %c",&jogador1);
-      printf("\n");
-      printf("Digite a escolha do segundo jogador: ");
-      scanf(" %c",&jogador2);
+      scanf(" %c",&jogador);
       printf("\n");
 
       // criando as condições
-      if((jogador1 == 'R' && jogador2 == 'S')||(jogador1 == 'P' && jogador2 == 'R')||(jogador1 == 'S' && jogador2 == 'P')){
-         printf("%s eh o Ganhador\n",nome1);
+      if((jogador == 'R' && jogada_bot() == 'S') || (jogador == 'S' && jogada_bot() == 'P') || (jogador == 'P' && jogada_bot() == 'R')){
+         printf("%s = %c e bot = %c\n",nome1,jogador,jogada_bot());
+         printf("\n");
+         printf("%s eh o vencedor\n");
          printf("\n");
       }
-      else if(jogador1 == jogador2){
-         printf("Empate :|\n");
+      else if(jogador == jogada_bot()){
+         printf("%s = %c e bot = %c\n",nome1,jogador,jogada_bot());
+         printf("\n");
+         printf("Empate\n");
          printf("\n");
       }
       else{
-         printf("%s eh o Ganhador\n",nome2);
+         printf("%s = %c e bot = %c\n",nome1,jogador,jogada_bot());
+         printf("\n");
+         printf("bot eh o vencedor\n");
          printf("\n");
       }
 
